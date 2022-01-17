61ec0afc3c6782106cacd585d3c952652a6c312a
su <nia1204@naver.com>
2022-01-17 17:09:39 +0900

    commit Ex3

--- DecJava/src/book/BookEx1.java
+++ DecJava/src/book/BookEx1.java
@@ -1,3 +1,5 @@
+package book;
+
 class BookEx1{
   public static void main(String[] args){
    System.out.println("test");  
---
+++ JavaTest/src/test/sample/Ex3.java
@@ -0,0 +1,10 @@
+package test.sample;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		System.out.println("test3");
+
+	}
+
+}
